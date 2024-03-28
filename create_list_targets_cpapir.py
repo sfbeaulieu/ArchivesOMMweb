9e52a600d684da925085da96270672d16652fbf6
Sylvie F Beaulieu <sfbeaulieu@gmail.com>
2024-03-28 16:46:06 -0400

    Update create_list_targets_cpapir.py

--- create_list_targets_cpapir.py
+++ create_list_targets_cpapir.py
@@ -16,10 +16,30 @@
 # les nouvelles nuits, et les nuits qui ont été réduites de nouveau pour y inclure les changements. 
 # La liste de compilation des nuits devra se réorganiser en ordre ascendant de "nom d'objet" (mais ça reste à discuter). 
 # Cette liste pourra être lu par le code php.
-# Un code php sera créé pour lire la liste, quelque chose de très simple pour débuter (la raison d'avoir une liste ascendante de "nom d'objet"), 
+# On devra faire rouler le code par instrument, et ensuite, combiner les listes pour n'en faire qu'une.
+# Un code php sera créé pour lire la liste complete (de tous les instruments), quelque chose de très simple pour débuter (la raison d'avoir une liste ascendante de "nom d'objet"), 
 # mais qui pourrait évoluer par la suite.
 
 # ce code plante... mais donne une idée de ce que l'on veut...
+
+# Il faut lire dans /data/cpapir/reductions/YYMMDD/YYMMDD_*.fits.gz (pour CPAPIR) (ensuite adapter le code pour PESTO etc...)
+# PLus rapide et directe que d'essayer de se créer une liste de nuits qui serait ensuite passé au code.
+
+# Surement qu'il y a une façon plus simple... mais bon, voilà ce dont nous avons besoin :
+
+# At the begining, we need to create the basic file : here, we need a question at the begining "Creating a basic file ?" if "Y" move to Boucle 1...
+# if "N", then move to Boucle 5
+# Boucle 1 : probe /data/cpapir/reductions/ and create a list of /data/cpapir/reductions/YYMMDD/YYMMDD_*.fits.gz (called basic_files_list_1)
+# Boucle 2 : from that list, probe the metadata, and create one "simple_header_YYMMDD_targetname_filter" table file per image, save in the image night folder
+# Boucle 3 : probe the /data/cpapir/reductions/YYMMDD/simple_header_YYMMDD_targetname_filter file and create a new list of all the table files "CPAPIR_simple_header_all"
+# Boucle 4 : open a table file called "archive_targets_omm" read CPAPIR_simple_header_all, and fetch the information in those indovodual tables, and dump in "archive_targets_omm"
+# Once we have run this once, stop
+# For new data, run de code, and answer "N" to the question at the begining
+# Boucle 5 : probe /data/cpapir/reductions/ and create a new list of /data/cpapir/reductions/YYMMDD/YYMMDD_*.fits.gz called "basic_files_list_2", 
+# and compare with the previous basic list, and create a file with new entries "new_entries"
+# Boucle 6 : read "new_entries" and probe the metadata, and create one "simple_header_YYMMDD_targetname_filter" table file per image, 
+# save in the image night folder, and add the entore path of that file to "CPAPIR_simple_header_all", and then, read the table file, and dump in "archive_targets_omm"
+
 
 from astropy.io import ascii
 from astropy.io import fits
