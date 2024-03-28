de3e74c18ed06c5fbb8340c4e790d689ab0aedbf
Sylvie F Beaulieu <sfbeaulieu@gmail.com>
2024-03-28 14:23:21 -0400

    Update create_list_targets_cpapir.py

--- create_list_targets_cpapir.py
+++ create_list_targets_cpapir.py
@@ -1,6 +1,18 @@
-#faire une boucle pour passer toutes les images dans une même nuit
-#dumper dans une table
-# ['OBJECT', 'RATXT ', 'DECTXT', 'TELESCOP','INSTRUME','FILTER','NIGHTID','EPOCH','RELEASE','N_IND (no. images indiv)','T_TOT (temps total)']
+# Code python pour lire une série de keywords dans le metadata des fichiers FITS pour créer une table qui sera accessible via une page web.
+# Le but est de pouvoir vérifier si une cible a déjà été observée, et si oui, avwec quel instrument et filtre.
+# Information qui sera tirée du metadata
+# keys = ["OBJECT", "RATXT", "DECTXT", "TELESCOP", "INSTRUME", "FILTER", N_IND", "T_TOT", "NIGHTID", "EPOCH", "RELEASE"]
+# RATXT et DECTXT sont les corrdonnées en format hh:mm:ss.s dd:mm:ss.s
+# N_IND : nombre de fichiers combinés pour former le produit final
+# T_TOT : Temps total du produit final
+
+# Le code passe à travers toutes les nuits YYMMDD sur la tour d'archives OMM
+# Exemple : pour CPAPIR, ce sont les images finales : /data/cpapir/reductions
+
+# La recherche sera mis dans une table ascii ou csv, et cette table sera lu par un code php
+
+# En premier lieu, toute l'archive sera lu, et par la suite, il faudrait trouver un moyen de lire l'archive pour 1) les nouvelles nuits, 2) les nuits qui ont été réduites de nouveau. 
+# La liste devra se réorganiser en ordre ascendant de "nom d'objet", mais ça reste à discuter.
 
 from astropy.io import ascii
 from astropy.io import fits
